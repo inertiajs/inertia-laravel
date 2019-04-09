dbec42d363a1541786aeb48edfe0b0ff1ebb7527
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2019-04-09 23:46:45 +0200

    Add Component methods as docblock properties to Intertia facade

--- src/Inertia.php
+++ src/Inertia.php
@@ -4,6 +4,13 @@ namespace Inertia;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Illuminate\Contracts\View\View render($component, $props = [])
+ * @method static array share($key, $value)
+ * @method static void setRootView($name)
+ *
+ * @see \Inertia\Component
+ */
 class Inertia extends Facade
 {
     protected static function getFacadeAccessor()
