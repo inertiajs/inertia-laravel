ebafc7ff70246bb206d77db23dac7bc1939fdb09
Jonathan Reinink <jonathan@reinink.ca>
2019-04-25 10:48:06 -0400

    Rename middleware and add redirect code correction

---
+++ src/Middleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Inertia;
+
+use Closure;
+use Illuminate\Support\Facades\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse as Redirect;
+
+class Middleware
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (!$request->header('X-Inertia')) {
+            return $response;
+        }
+
+        if ($request->method() === 'GET' && $request->header('X-Inertia-Version') !== Inertia::getVersion()) {
+            return Response::make('', 409, ['X-Inertia-Location' => $request->fullUrl()]);
+        }
+
+        if ($response instanceof Redirect && $response->getStatusCode() === 302 && in_array($request->method(), ['PUT', 'PATCH', 'DELETE'])) {
+            $response->setStatusCode(303);
+        }
+
+        return $response;
+    }
+}
