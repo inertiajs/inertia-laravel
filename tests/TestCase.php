6067b7ca0abee680bdc2992967037e32535c868d
Jonathan Reinink <jonathan@reinink.ca>
2019-05-21 15:18:08 -0400

    Add basic tests

---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Support\Facades\View;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    protected function getPackageProviders($app)
+    {
+        return ['Inertia\ServiceProvider'];
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        View::addLocation(__DIR__.'/views');
+    }
+}
