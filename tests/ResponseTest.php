9e70fb879448059965fa80fe24c56fbd3c961277
Jonathan Reinink <jonathan@reinink.ca>
2019-05-21 15:18:08 -0400

    Add basic tests

---
+++ tests/ResponseTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests;
+
+use Inertia\Response;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class ResponseTest extends TestCase
+{
+    public function test_server_response()
+    {
+        $request = Request::create('/user/123', 'GET');
+
+        $response = new Response(
+            'User/Edit',
+            ['user' => ['name' => 'Jonathan']],
+            'app',
+            ['foo' => 'bar'],
+            '123'
+        );
+
+        $response = $response->toResponse($request);
+        $page = $response->getData()['page'];
+
+        $this->assertInstanceOf(View::class, $response);
+        $this->assertSame('User/Edit', $page['component']);
+        $this->assertSame('Jonathan', $page['props']['user']['name']);
+        $this->assertSame('bar', $page['props']['foo']);
+        $this->assertSame('/user/123', $page['url']);
+        $this->assertSame('123', $page['version']);
+        $this->assertSame('<div id="app" data-page="{&quot;component&quot;:&quot;User\/Edit&quot;,&quot;props&quot;:{&quot;foo&quot;:&quot;bar&quot;,&quot;user&quot;:{&quot;name&quot;:&quot;Jonathan&quot;}},&quot;url&quot;:&quot;\/user\/123&quot;,&quot;version&quot;:&quot;123&quot;}"></div>'."\n", $response->render());
+    }
+
+    public function test_xhr_response()
+    {
+        $request = Request::create('/user/123', 'GET');
+        $request->headers->add(['X-Inertia' => 'true']);
+
+        $response = new Response(
+            'User/Edit',
+            ['user' => ['name' => 'Jonathan']],
+            'app',
+            ['foo' => 'bar'],
+            '123'
+        );
+
+        $response = $response->toResponse($request);
+        $page = $response->getData();
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertSame('User/Edit', $page->component);
+        $this->assertSame('Jonathan', $page->props->user->name);
+        $this->assertSame('bar', $page->props->foo);
+        $this->assertSame('/user/123', $page->url);
+        $this->assertSame('123', $page->version);
+    }
+}
