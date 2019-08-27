d813aa3fa306e58b6fbb111461a402a27850e5cc
Jonathan Reinink <jonathan@reinink.ca>
2019-08-27 07:05:25 -0400

    Auto register middleware

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,24 +3,38 @@
 namespace Inertia;
 
 use Illuminate\Routing\Router;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    /**
-     * Perform post-registration booting of services.
-     */
     public function boot()
+    {
+        $this->registerBladeDirective();
+        $this->registerRouterMacro();
+        $this->registerMiddleware();
+    }
+
+    protected function registerBladeDirective()
     {
         Blade::directive('inertia', function () {
             return '<div id="app" data-page="{{ json_encode($page) }}"></div>';
         });
+    }
 
+    protected function registerRouterMacro()
+    {
         Router::macro('inertia', function ($uri, $component, $props = []) {
             return $this->match(['GET', 'HEAD'], $uri, '\Inertia\Controller')
                 ->defaults('component', $component)
                 ->defaults('props', $props);
         });
+    }
+
+    protected function registerMiddleware()
+    {
+        $kernel = $this->app[Kernel::class];
+        $kernel->pushMiddleware(Middleware::class);
     }
 }
