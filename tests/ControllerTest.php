e2af1dd0556b1222bcef40868ff04022359d7bb8
Ilya Sakovich <me@hivokas.com>
2019-08-28 22:56:37 +0300

    tests

--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -4,22 +4,13 @@ namespace Inertia\Tests;
 
 use Inertia\Response;
 use Inertia\Controller;
-use Illuminate\Http\Request;
 
 class ControllerTest extends TestCase
 {
     public function test_controller_returns_an_inertia_response()
     {
-        $response = (new Controller())('User/Edit', ['user' => ['name' => 'Jonathan']]);
+        $inertiaResponse = (new Controller())('User/Edit', ['user' => ['name' => 'Jonathan']]);
 
-        $this->assertInstanceOf(Response::class, $response);
-        $this->assertEquals([
-            'page' => [
-                'component' => 'User/Edit',
-                'props' => ['user' => ['name' => 'Jonathan']],
-                'url' => '',
-                'version' => null,
-            ],
-        ], $response->toResponse(new Request())->getData());
+        $this->assertInstanceOf(Response::class, $inertiaResponse);
     }
 }
--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -3,9 +3,9 @@
 namespace Inertia\Tests;
 
 use Inertia\Response;
-use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response as HttpResponse;
 
 class ResponseTest extends TestCase
 {
@@ -13,22 +13,20 @@ class ResponseTest extends TestCase
     {
         $request = Request::create('/user/123', 'GET');
 
-        $response = new Response(
+        $inertiaResponse = new Response(
             'User/Edit',
             ['user' => ['name' => 'Jonathan']],
             'app',
             '123'
         );
 
-        $response = $response->toResponse($request);
-        $page = $response->getData()['page'];
+        $inertiaResponse->withStatusCode(418);
 
-        $this->assertInstanceOf(View::class, $response);
-        $this->assertSame('User/Edit', $page['component']);
-        $this->assertSame('Jonathan', $page['props']['user']['name']);
-        $this->assertSame('/user/123', $page['url']);
-        $this->assertSame('123', $page['version']);
-        $this->assertSame('<div id="app" data-page="{&quot;component&quot;:&quot;User\/Edit&quot;,&quot;props&quot;:{&quot;user&quot;:{&quot;name&quot;:&quot;Jonathan&quot;}},&quot;url&quot;:&quot;\/user\/123&quot;,&quot;version&quot;:&quot;123&quot;}"></div>'."\n", $response->render());
+        $httpResponse = $inertiaResponse->toResponse($request);
+
+        $this->assertInstanceOf(HttpResponse::class, $httpResponse);
+        $this->assertSame(418, $httpResponse->getStatusCode());
+        $this->assertSame('<div id="app" data-page="{&quot;component&quot;:&quot;User\/Edit&quot;,&quot;props&quot;:{&quot;user&quot;:{&quot;name&quot;:&quot;Jonathan&quot;}},&quot;url&quot;:&quot;\/user\/123&quot;,&quot;version&quot;:&quot;123&quot;}"></div>'.PHP_EOL, $httpResponse->getContent());
     }
 
     public function test_xhr_response()
@@ -36,17 +34,21 @@ class ResponseTest extends TestCase
         $request = Request::create('/user/123', 'GET');
         $request->headers->add(['X-Inertia' => 'true']);
 
-        $response = new Response(
+        $inertiaResponse = new Response(
             'User/Edit',
             ['user' => ['name' => 'Jonathan']],
             'app',
             '123'
         );
 
-        $response = $response->toResponse($request);
-        $page = $response->getData();
+        $inertiaResponse->withStatusCode(418);
 
-        $this->assertInstanceOf(JsonResponse::class, $response);
+        $jsonResponse = $inertiaResponse->toResponse($request);
+
+        $page = $jsonResponse->getData();
+
+        $this->assertInstanceOf(JsonResponse::class, $jsonResponse);
+        $this->assertSame(418, $jsonResponse->getStatusCode());
         $this->assertSame('User/Edit', $page->component);
         $this->assertSame('Jonathan', $page->props->user->name);
         $this->assertSame('/user/123', $page->url);
