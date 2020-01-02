f07655735c646bb038aaaddc55c567444a1a7773
Jonathan Reinink <jonathan@reinink.ca>
2020-01-02 11:38:28 -0500

    Switch method order in service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,17 +10,17 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
+    public function register()
+    {
+        $this->app->singleton(ResponseFactory::class);
+    }
+
     public function boot()
     {
         $this->registerBladeDirective();
         $this->registerRequestMacro();
         $this->registerRouterMacro();
         $this->registerMiddleware();
-    }
-
-    public function register()
-    {
-        $this->app->singleton(ResponseFactory::class);
     }
 
     protected function registerBladeDirective()
