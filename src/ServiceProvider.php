e684e250e55b94090e57a8e71f420d30f941a6ea
Jonathan Reinink <jonathan@reinink.ca>
2020-09-01 07:18:32 -0400

    Automatically share validation errors

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
@@ -21,6 +22,7 @@ class ServiceProvider extends BaseServiceProvider
         $this->registerRequestMacro();
         $this->registerRouterMacro();
         $this->registerMiddleware();
+        $this->shareValidationErrors();
     }
 
     protected function registerBladeDirective()
@@ -49,5 +51,18 @@ class ServiceProvider extends BaseServiceProvider
     protected function registerMiddleware()
     {
         $this->app[Kernel::class]->pushMiddleware(Middleware::class);
+    }
+
+    protected function shareValidationErrors()
+    {
+        if (Inertia::getShared('errors')) {
+            return;
+        }
+
+        Inertia::share('errors', function () {
+            return (object) array_map(function ($error) {
+                return $error[0];
+            }, Session::has('errors') ? Session::get('errors')->messages() : []);
+        });
     }
 }
