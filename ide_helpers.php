ecb5328a890cff2467a8b51c53c9b32d12a6057e
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-09-02 19:06:06 +0200

    Add IDE autocompletion helpers

---
+++ ide_helpers.php
@@ -0,0 +1,41 @@
+<?php
+
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+/** @noinspection PhpUnusedAliasInspection */
+
+namespace Illuminate\Testing {
+
+    /**
+     * @see \Inertia\Assertions
+     *
+     * @method self assertInertia()
+     * @method self assertInertiaComponent($name)
+     * @method self assertInertiaHas($key, $value = null)
+     * @method self assertInertiaHasAll(array $bindings)
+     * @method self assertInertiaMissing($key)
+     * @method array inertiaProps()
+     */
+    class TestResponse
+    {
+        //
+    }
+}
+
+namespace Illuminate\Foundation\Testing {
+
+    /**
+     * @see \Inertia\Assertions
+     *
+     * @method self assertInertia()
+     * @method self assertInertiaComponent($name)
+     * @method self assertInertiaHas($key, $value = null)
+     * @method self assertInertiaHasAll(array $bindings)
+     * @method self assertInertiaMissing($key)
+     * @method array inertiaProps()
+     */
+    class TestResponse
+    {
+        //
+    }
+}
