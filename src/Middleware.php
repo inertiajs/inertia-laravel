a2152db81b36a33a0fee59d80c3d053769354a82
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2020-10-10 13:43:54 +0200

    Update src/Middleware.php

--- src/Middleware.php
+++ src/Middleware.php
@@ -19,7 +19,18 @@ abstract class Middleware
      */
     public function version(Request $request)
     {
-        //
+        // When we are running on Laravel Vapor, asset URLs are automatically updated
+        // when the application is being deployed. Because of this, we do not need
+        // to use any files for hashing, as we can simply use this URL instead.
+        if (config('app.asset_url')) {
+            return md5(config('app.asset_url'));
+        }
+
+        // Alternatively, when we are running in a regular (non-serverless) environment
+        // we'll attempt to use the Laravel Mix asset manifest to generate our hash.
+        if (file_exists($manifest = public_path('mix-manifest.json'))) {
+            return md5_file($manifest);
+        }
     }
 
     /**
