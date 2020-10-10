cb45c3f04a99e187ce6f0feeee5596c61e2afd18
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-10-10 13:51:34 +0200

    Fix remarks & enable versioning by default

--- src/Middleware.php
+++ src/Middleware.php
@@ -8,10 +8,10 @@ use Illuminate\Support\Collection;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-abstract class Middleware
+class Middleware
 {
     /**
-     * Determines the current Inertia asset version hash.
+     * Determines the current asset version.
      *
      * @see https://inertiajs.com/asset-versioning
      * @param  \Illuminate\Http\Request  $request
