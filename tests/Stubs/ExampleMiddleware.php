ffc973b0d631c5e63bd9d1a5b0604fb4718b3cf0
Claudio Dekker <claudiodekker@users.noreply.github.com>
2021-11-30 14:32:09 +0000

    Apply fixes from StyleCI

--- tests/Stubs/ExampleMiddleware.php
+++ tests/Stubs/ExampleMiddleware.php
@@ -26,6 +26,7 @@ class ExampleMiddleware extends Middleware
      * Determines the current asset version.
      *
      * @see https://inertiajs.com/asset-versioning
+     *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
@@ -38,6 +39,7 @@ class ExampleMiddleware extends Middleware
      * Defines the props that are shared by default.
      *
      * @see https://inertiajs.com/shared-data
+     *
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
