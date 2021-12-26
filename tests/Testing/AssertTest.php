16b8733bcdd80047e33ec04e203b9166baf299a9
Claudio Dekker <claudio@ubient.net>
2021-12-26 18:10:29 +0100

    Migrate to AssertableJson (Laravel-core)

--- tests/Testing/AssertTest.php
+++ tests/Testing/AssertTest.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Collection;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Inertia\Inertia;
 use Inertia\Testing\Assert;
 use Inertia\Tests\TestCase;
@@ -15,146 +16,13 @@ use TypeError;
 
 class AssertTest extends TestCase
 {
-    /** @test */
-    public function the_view_is_served_by_inertia(): void
+    public function setUp(): void
     {
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
+        parent::setUp();
 
-        $response->assertInertia();
-    }
-
-    /** @test */
-    public function the_view_is_not_served_by_inertia(): void
-    {
-        $response = $this->makeMockRequest(view('welcome'));
-        $response->assertOk(); // Make sure we can render the built-in Orchestra 'welcome' view..
-
-        $this->expectException(AssertionFailedError::class);
-        $this->expectExceptionMessage('Not a valid Inertia response.');
-
-        $response->assertInertia();
-    }
-
-    /** @test */
-    public function the_component_matches(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->component('foo');
-        });
-    }
-
-    /** @test */
-    public function the_component_does_not_match(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        $this->expectException(AssertionFailedError::class);
-        $this->expectExceptionMessage('Unexpected Inertia page component.');
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->component('bar');
-        });
-    }
-
-    /** @test */
-    public function the_component_exists_on_the_filesystem(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('Stubs/ExamplePage')
-        );
-
-        config()->set('inertia.testing.ensure_pages_exist', true);
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->component('Stubs/ExamplePage');
-        });
-    }
-
-    /** @test */
-    public function the_component_does_not_exist_on_the_filesystem(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        config()->set('inertia.testing.ensure_pages_exist', true);
-        $this->expectException(AssertionFailedError::class);
-        $this->expectExceptionMessage('Inertia page component file [foo] does not exist.');
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->component('foo');
-        });
-    }
-
-    /** @test */
-    public function it_can_force_enable_the_component_file_existence(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        config()->set('inertia.testing.ensure_pages_exist', false);
-        $this->expectException(AssertionFailedError::class);
-        $this->expectExceptionMessage('Inertia page component file [foo] does not exist.');
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->component('foo', true);
-        });
-    }
-
-    /** @test */
-    public function it_can_force_disable_the_component_file_existence_check(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        config()->set('inertia.testing.ensure_pages_exist', true);
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->component('foo', false);
-        });
-    }
-
-    /** @test */
-    public function the_component_does_not_exist_on_the_filesystem_when_it_does_not_exist_relative_to_any_of_the_given_paths(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('fixtures/ExamplePage')
-        );
-
-        config()->set('inertia.testing.ensure_pages_exist', true);
-        config()->set('inertia.testing.page_paths', [realpath(__DIR__)]);
-        $this->expectException(AssertionFailedError::class);
-        $this->expectExceptionMessage('Inertia page component file [fixtures/ExamplePage] does not exist.');
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->component('fixtures/ExamplePage');
-        });
-    }
-
-    /** @test */
-    public function the_component_does_not_exist_on_the_filesystem_when_it_does_not_have_one_of_the_configured_extensions(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('fixtures/ExamplePage')
-        );
-
-        config()->set('inertia.testing.ensure_pages_exist', true);
-        config()->set('inertia.testing.page_extensions', ['bin', 'exe', 'svg']);
-        $this->expectException(AssertionFailedError::class);
-        $this->expectExceptionMessage('Inertia page component file [fixtures/ExamplePage] does not exist.');
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->component('fixtures/ExamplePage');
-        });
+        if (class_exists(AssertableJson::class)) {
+            $this->markTestSkipped("These tests are not applicable on Laravel 8.32 or newer, as Laravel's built-in AssertableJson is used instead.");
+        }
     }
 
     /** @test */
@@ -1199,64 +1067,6 @@ class AssertTest extends TestCase
                 'bar' => 2,
                 'baz' => 1,
             ]);
-        });
-    }
-
-    /** @test */
-    public function the_page_url_matches(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->url('/example-url');
-        });
-    }
-
-    /** @test */
-    public function the_page_url_does_not_match(): void
-    {
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        $this->expectException(AssertionFailedError::class);
-        $this->expectExceptionMessage('Unexpected Inertia page url.');
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->url('/invalid-page');
-        });
-    }
-
-    /** @test */
-    public function the_asset_version_matches(): void
-    {
-        Inertia::version('example-version');
-
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->version('example-version');
-        });
-    }
-
-    /** @test */
-    public function the_asset_version_does_not_match(): void
-    {
-        Inertia::version('example-version');
-
-        $response = $this->makeMockRequest(
-            Inertia::render('foo')
-        );
-
-        $this->expectException(AssertionFailedError::class);
-        $this->expectExceptionMessage('Unexpected Inertia asset version.');
-
-        $response->assertInertia(function (Assert $inertia) {
-            $inertia->version('different-version');
         });
     }
 
