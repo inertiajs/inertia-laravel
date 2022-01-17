3ab0f1a9644e065632b5eafe676c68068eacf985
Rhys Emmerson <rhyses0107360@gmail.com>
2022-01-18 00:46:31 +1000

    Fix incorrect type hint in render method (#353)
    Co-authored-by: Rhys <rhys@iseekplant.com.au>

--- src/ResponseFactory.php
+++ src/ResponseFactory.php
@@ -85,7 +85,11 @@ class ResponseFactory
         return new LazyProp($callback);
     }
 
-    public function render($component, array $props = []): Response
+    /**
+     * @param  string  $component
+     * @param  array|Arrayable  $props
+     */
+    public function render($component, $props = []): Response
     {
         if ($props instanceof Arrayable) {
             $props = $props->toArray();
