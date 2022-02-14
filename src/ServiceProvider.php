inertiajs/inertia-laravel

commit 667335e06316366963e1c2bf4e6618229e2ffade

[SuperDJ] Added missing types (#301) (#337)
* Added typings

Co-authored-by: SuperDJ <super.derkjan@gmail.com>

src/ServiceProvider.php
@@ -11,10 +11,11 @@ use Illuminate\Testing\TestResponse;
 use Illuminate\View\FileViewFinder;
 use Inertia\Testing\TestResponseMacros;
 use LogicException;
+use ReflectionException;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    public function register()
+    public function register(): void
     {
         $this->app->singleton(ResponseFactory::class);
 
@@ -36,7 +37,7 @@ class ServiceProvider extends BaseServiceProvider
         });
     }
 
-    public function boot()
+    public function boot(): void
     {
         $this->registerBladeDirective();
         $this->registerConsoleCommands();
@@ -46,14 +47,14 @@ class ServiceProvider extends BaseServiceProvider
         ]);
     }
 
-    protected function registerBladeDirective()
+    protected function registerBladeDirective(): void
     {
         Blade::directive('inertia', function () {
             return '<div id="app" data-page="{{ json_encode($page) }}"></div>';
         });
     }
 
-    protected function registerConsoleCommands()
+    protected function registerConsoleCommands(): void
     {
         if (! $this->app->runningInConsole()) {
             return;
@@ -64,23 +65,26 @@ class ServiceProvider extends BaseServiceProvider
         ]);
     }
 
-    protected function registerRequestMacro()
+    protected function registerRequestMacro(): void
     {
         Request::macro('inertia', function () {
-            return boolval($this->header('X-Inertia'));
+            return (bool) $this->header('X-Inertia');
         });
     }
 
-    protected function registerRouterMacro()
+    protected function registerRouterMacro(): void
     {
         Router::macro('inertia', function ($uri, $component, $props = []) {
-            return $this->match(['GET', 'HEAD'], $uri, '\Inertia\Controller')
+            return $this->match(['GET', 'HEAD'], $uri, Controller::class)
                 ->defaults('component', $component)
                 ->defaults('props', $props);
         });
     }
 
-    protected function registerTestingMacros()
+    /**
+     * @throws ReflectionException|LogicException
+     */
+    protected function registerTestingMacros(): void
     {
         if (class_exists(TestResponse::class)) {
             TestResponse::mixin(new TestResponseMacros());

tests/ServiceProviderTest.php
@@ -5,10 +5,11 @@ namespace Inertia\Tests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
+use Inertia\Controller;
 
 class ServiceProviderTest extends TestCase
 {
-    public function test_blade_directive_is_registered()
+    public function test_blade_directive_is_registered(): void
     {
         $directives = Blade::getCustomDirectives();
 
@@ -16,7 +17,7 @@ class ServiceProviderTest extends TestCase
         $this->assertEquals('<div id="app" data-page="{{ json_encode($page) }}"></div>', $directives['inertia']());
     }
 
-    public function test_request_macro_is_registered()
+    public function test_request_macro_is_registered(): void
     {
         $request = Request::create('/user/123', 'GET');
 
@@ -27,7 +28,7 @@ class ServiceProviderTest extends TestCase
         $this->assertTrue($request->inertia());
     }
 
-    public function test_route_macro_is_registered()
+    public function test_route_macro_is_registered(): void
     {
         $route = Route::inertia('/', 'User/Edit', ['user' => ['name' => 'Jonathan']]);
         $routes = Route::getRoutes();
@@ -36,7 +37,7 @@ class ServiceProviderTest extends TestCase
         $this->assertEquals($route, $routes->getRoutes()[0]);
         $this->assertEquals(['GET', 'HEAD'], $route->methods);
         $this->assertEquals('/', $route->uri);
-        $this->assertEquals(['uses' => '\Inertia\Controller@__invoke', 'controller' => '\Inertia\Controller'], $route->action);
+        $this->assertEquals(['uses' => 'Inertia\Controller@__invoke', 'controller' => Controller::class], $route->action);
         $this->assertEquals(['component' => 'User/Edit', 'props' => ['user' => ['name' => 'Jonathan']]], $route->defaults);
     }
 }

src/Console/CreateMiddleware.php
@@ -33,7 +33,7 @@ class CreateMiddleware extends GeneratorCommand
      *
      * @return string
      */
-    protected function getStub()
+    protected function getStub(): string
     {
         return __DIR__.'/../../stubs/middleware.stub';
     }
@@ -44,7 +44,7 @@ class CreateMiddleware extends GeneratorCommand
      * @param  string  $rootNamespace
      * @return string
      */
-    protected function getDefaultNamespace($rootNamespace)
+    protected function getDefaultNamespace($rootNamespace): string
     {
         return $rootNamespace.'\Http\Middleware';
     }
@@ -54,7 +54,7 @@ class CreateMiddleware extends GeneratorCommand
      *
      * @return array
      */
-    protected function getArguments()
+    protected function getArguments(): array
     {
         return [
             ['name', InputOption::VALUE_REQUIRED, 'Name of the Middleware that should be created', 'HandleInertiaRequests'],
@@ -66,7 +66,7 @@ class CreateMiddleware extends GeneratorCommand
      *
      * @return array
      */
-    protected function getOptions()
+    protected function getOptions(): array
     {
         return [
             ['force', null, InputOption::VALUE_NONE, 'Create the class even if the Middleware already exists'],

src/Controller.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): Response
     {
         return Inertia::render(
             $request->route()->defaults['component'],

src/Inertia.php
@@ -5,20 +5,20 @@ namespace Inertia;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static void setRootView($name)
+ * @method static void setRootView(string $name)
  * @method static void share($key, $value = null)
  * @method static array getShared($key = null, $default = null)
  * @method static void version($version)
  * @method static int|string getVersion()
- * @method static Response render($component, $props = [])
- * @method static \Illuminate\Http\Response location($url)
+ * @method static Response render($component, array $props = [])
+ * @method static \Illuminate\Http\Response location(string $url)
  * @method static LazyProp lazy(callable $callback)
  *
  * @see \Inertia\ResponseFactory
  */
 class Inertia extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return ResponseFactory::class;
     }

src/Middleware.php
@@ -25,7 +25,7 @@ class Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    public function version(Request $request)
+    public function version(Request $request): ?string
     {
         if (config('app.asset_url')) {
             return md5(config('app.asset_url'));
@@ -34,6 +34,8 @@ class Middleware
         if (file_exists($manifest = public_path('mix-manifest.json'))) {
             return md5_file($manifest);
         }
+
+        return null;
     }
 
     /**
@@ -44,7 +46,7 @@ class Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    public function share(Request $request)
+    public function share(Request $request): array
     {
         return [
             'errors' => function () use ($request) {
@@ -61,7 +63,7 @@ class Middleware
      * @param  Request  $request
      * @return string
      */
-    public function rootView(Request $request)
+    public function rootView(Request $request): string
     {
         return $this->rootView;
     }
@@ -73,7 +75,7 @@ class Middleware
      * @param  Closure  $next
      * @return Response
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         Inertia::version(function () use ($request) {
             return $this->version($request);
@@ -85,9 +87,8 @@ class Middleware
 
         $response = $next($request);
         $response = $this->checkVersion($request, $response);
-        $response = $this->changeRedirectCode($request, $response);
 
-        return $response;
+        return $this->changeRedirectCode($request, $response);
     }
 
     /**
@@ -98,7 +99,7 @@ class Middleware
      * @param  Response  $response
      * @return Response
      */
-    public function checkVersion(Request $request, Response $response)
+    public function checkVersion(Request $request, Response $response): Response
     {
         if ($request->header('X-Inertia') &&
             $request->method() === 'GET' &&
@@ -122,7 +123,7 @@ class Middleware
      * @param  Response  $response
      * @return Response
      */
-    public function changeRedirectCode(Request $request, Response $response)
+    public function changeRedirectCode(Request $request, Response $response): Response
     {
         if ($request->header('X-Inertia') &&
             $response->getStatusCode() === 302 &&
@@ -141,7 +142,7 @@ class Middleware
      * @param  Request  $request
      * @return object
      */
-    public function resolveValidationErrors(Request $request)
+    public function resolveValidationErrors(Request $request): object
     {
         if (! $request->session()->has('errors')) {
             return (object) [];
@@ -154,11 +155,13 @@ class Middleware
         })->pipe(function ($bags) use ($request) {
             if ($bags->has('default') && $request->header('x-inertia-error-bag')) {
                 return [$request->header('x-inertia-error-bag') => $bags->get('default')];
-            } elseif ($bags->has('default')) {
-                return $bags->get('default');
-            } else {
-                return $bags->toArray();
             }
+
+            if ($bags->has('default')) {
+                return $bags->get('default');
+            }
+
+            return $bags->toArray();
         });
     }
 }

src/Response.php
@@ -23,7 +23,13 @@ class Response implements Responsable
     protected $version;
     protected $viewData = [];
 
-    public function __construct($component, $props, $rootView = 'app', $version = null)
+    /**
+     * @param  string  $component
+     * @param  array|Arrayable  $props
+     * @param  string  $rootView
+     * @param  string  $version
+     */
+    public function __construct(string $component, $props, string $rootView = 'app', string $version = '')
     {
         $this->component = $component;
         $this->props = $props instanceof Arrayable ? $props->toArray() : $props;
@@ -31,7 +37,12 @@ class Response implements Responsable
         $this->version = $version;
     }
 
-    public function with($key, $value = null)
+    /**
+     * @param  string|array  $key
+     * @param  mixed|null  $value
+     * @return $this
+     */
+    public function with($key, $value = null): self
     {
         if (is_array($key)) {
             $this->props = array_merge($this->props, $key);
@@ -42,7 +53,12 @@ class Response implements Responsable
         return $this;
     }
 
-    public function withViewData($key, $value = null)
+    /**
+     * @param  string|array  $key
+     * @param  mixed|null  $value
+     * @return $this
+     */
+    public function withViewData($key, $value = null): self
     {
         if (is_array($key)) {
             $this->viewData = array_merge($this->viewData, $key);
@@ -53,24 +69,30 @@ class Response implements Responsable
         return $this;
     }
 
-    public function rootView($rootView)
+    public function rootView(string $rootView): self
     {
         $this->rootView = $rootView;
 
         return $this;
     }
 
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function toResponse($request)
     {
         $only = array_filter(explode(',', $request->header('X-Inertia-Partial-Data', '')));
 
         $props = ($only && $request->header('X-Inertia-Partial-Component') === $this->component)
             ? Arr::only($this->props, $only)
-            : array_filter($this->props, function ($prop) {
+            : array_filter($this->props, static function ($prop) {
                 return ! ($prop instanceof LazyProp);
             });
 
-        array_walk_recursive($props, function (&$prop) use ($request) {
+        array_walk_recursive($props, static function (&$prop) use ($request) {
             if ($prop instanceof LazyProp) {
                 $prop = App::call($prop);
             }

src/ResponseFactory.php
@@ -15,16 +15,25 @@ class ResponseFactory
 {
     use Macroable;
 
+    /** @var string */
     protected $rootView = 'app';
-    protected $sharedProps = [];
-    protected $version = null;
 
-    public function setRootView($name)
+    /** @var array */
+    protected $sharedProps = [];
+
+    /** @var Closure|string|null */
+    protected $version;
+
+    public function setRootView(string $name): void
     {
         $this->rootView = $name;
     }
 
-    public function share($key, $value = null)
+    /**
+     * @param  string|array|Arrayable  $key
+     * @param  mixed|null  $value
+     */
+    public function share($key, $value = null): void
     {
         if (is_array($key)) {
             $this->sharedProps = array_merge($this->sharedProps, $key);
@@ -35,7 +44,12 @@ class ResponseFactory
         }
     }
 
-    public function getShared($key = null, $default = null)
+    /**
+     * @param  string|null  $key
+     * @param  null|mixed  $default
+     * @return mixed
+     */
+    public function getShared(string $key = null, $default = null)
     {
         if ($key) {
             return Arr::get($this->sharedProps, $key, $default);
@@ -44,17 +58,20 @@ class ResponseFactory
         return $this->sharedProps;
     }
 
-    public function flushShared()
+    public function flushShared(): void
     {
         $this->sharedProps = [];
     }
 
-    public function version($version)
+    /**
+     * @param  Closure|string|null  $version
+     */
+    public function version($version): void
     {
         $this->version = $version;
     }
 
-    public function getVersion()
+    public function getVersion(): string
     {
         $version = $this->version instanceof Closure
             ? App::call($this->version)
@@ -63,12 +80,12 @@ class ResponseFactory
         return (string) $version;
     }
 
-    public function lazy(callable $callback)
+    public function lazy(callable $callback): LazyProp
     {
         return new LazyProp($callback);
     }
 
-    public function render($component, $props = [])
+    public function render($component, array $props = []): Response
     {
         if ($props instanceof Arrayable) {
             $props = $props->toArray();
@@ -82,7 +99,10 @@ class ResponseFactory
         );
     }
 
-    public function location($url)
+    /**
+     * @param  string|RedirectResponse  $url
+     */
+    public function location($url): \Symfony\Component\HttpFoundation\Response
     {
         if ($url instanceof RedirectResponse) {
             $url = $url->getTargetUrl();

commit deec28140049d270e8ea25ad93e42533d03e45bd

Fix regression introduced in #337

src/ServiceProvider.php
@@ -77,7 +77,7 @@ class ServiceProvider extends BaseServiceProvider
     protected function registerRouterMacro(): void
     {
         Router::macro('inertia', function ($uri, $component, $props = []) {
-            return $this->match(['GET', 'HEAD'], $uri, Controller::class)
+            return $this->match(['GET', 'HEAD'], $uri, '\Inertia\Controller')
                 ->defaults('component', $component)
                 ->defaults('props', $props);
         });

tests/ServiceProviderTest.php
@@ -5,7 +5,6 @@ namespace Inertia\Tests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
-use Inertia\Controller;
 
 class ServiceProviderTest extends TestCase
 {
@@ -34,7 +33,7 @@ class ServiceProviderTest extends TestCase
         $this->assertEquals($route, $routes->getRoutes()[0]);
         $this->assertEquals(['GET', 'HEAD'], $route->methods);
         $this->assertEquals('/', $route->uri);
-        $this->assertEquals(['uses' => 'Inertia\Controller@__invoke', 'controller' => Controller::class], $route->action);
+        $this->assertEquals(['uses' => '\Inertia\Controller@__invoke', 'controller' => '\Inertia\Controller'], $route->action);
         $this->assertEquals(['component' => 'User/Edit', 'props' => ['user' => ['name' => 'Jonathan']]], $route->defaults);
     }
 }