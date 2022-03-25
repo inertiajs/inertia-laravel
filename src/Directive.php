0d12b2944268a782bae7cd90189c5b4423741c39
Nurdism <nurdism.io@gmail.com>
2022-03-25 13:09:38 -0700

    Fix styleci issues

--- src/Directive.php
+++ src/Directive.php
@@ -13,7 +13,7 @@ class Directive
     public static function compile($expression = ''): string
     {
         $args = explode(',', $expression);
-        $id = !empty($args[0]) ?  trim(trim($args[0]), "\'\"") : 'app';
+        $id = ! empty($args[0]) ? trim(trim($args[0]), "\'\"") : 'app';
         $classes = isset($args[1]) ? trim(trim($args[1]), "\'\"") : '';
 
         $template = '<?php
@@ -24,7 +24,7 @@ class Directive
             if ($__inertiaSsr instanceof \Inertia\Ssr\Response) {
                 echo $__inertiaSsr->body;
             } else {
-                ?><div id="'.$id.'" ' . (!empty($classes) ? 'class="' . $classes . '"' : '') . 'data-page="{{ json_encode($page) }}"></div><?php
+                ?><div id="'.$id.'" '.(! empty($classes) ? 'class="'.$classes.'" ' : '').'data-page="{{ json_encode($page) }}"></div><?php
             }
         ?>';
 
