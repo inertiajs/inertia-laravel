ffecc421c74d1276e8e702e2886652621cc3e9fa
Jonathan Reinink <jonathan@reinink.ca>
2023-01-12 21:21:03 -0500

    Auto enable SSR based on existence of SSR bundle

--- config/inertia.php
+++ config/inertia.php
@@ -10,6 +10,9 @@ return [
     | These options configures if and how Inertia uses Server Side Rendering
     | to pre-render the initial visits made to your application's pages.
     |
+    | You can specify a custom SSR bundle path, or omit it to let Inertia
+    | try and automatically detect it for you.
+    |
     | Do note that enabling these options will NOT automatically make SSR work,
     | as a separate rendering service needs to be available. To learn more,
     | please visit https://inertiajs.com/server-side-rendering
@@ -18,11 +21,9 @@ return [
 
     'ssr' => [
 
-        'enabled' => false,
-
         'url' => 'http://127.0.0.1:13714',
 
-        'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
+        // 'bundle' => base_path('bootstrap/ssr/ssr.mjs')
 
     ],
 
--- src/Commands/StartSsr.php
+++ src/Commands/StartSsr.php
@@ -2,7 +2,9 @@
 
 namespace Inertia\Commands;
 
+use Inertia\Ssr\SsrException;
 use Illuminate\Console\Command;
+use Inertia\Ssr\BundleDetector;
 use Symfony\Component\Process\Process;
 
 class StartSsr extends Command
@@ -26,16 +28,23 @@ class StartSsr extends Command
      */
     public function handle(): int
     {
-        $ssrBundle = config('inertia.ssr.bundle', base_path('bootstrap/ssr/ssr.mjs'));
+        $bundle = (new BundleDetector())->detect();
+        $configuredBundle = config('inertia.ssr.bundle');
 
-        if (! file_exists($ssrBundle)) {
-            $this->error('Inertia SSR bundle not found: '.$ssrBundle);
-            $this->info('Set the correct Inertia SSR bundle path in your `inertia.ssr.bundle` config.');
+        if ($bundle === null) {
+            $this->error(
+                $configuredBundle
+                    ? 'Inertia SSR bundle not found at the configured path: "'.$configuredBundle.'"'
+                    : 'Inertia SSR bundle not found. Set the correct Inertia SSR bundle path in your `inertia.ssr.bundle` config.'
+            );
 
             return self::FAILURE;
+        } elseif ($configuredBundle && $bundle !== $configuredBundle) {
+            $this->warn('Inertia SSR bundle not found at the configured path: "'.$configuredBundle.'"');
+            $this->warn('Using a default bundle instead: "'.$bundle.'"');
         }
 
-        $process = new Process(['node', $ssrBundle]);
+        $process = new Process(['node', $bundle]);
         $process->setTimeout(null);
         $process->start();
 
