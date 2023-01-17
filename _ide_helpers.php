748ed2b1b133385897910b96db23840818b2fc9b
Lucas Yang <yangchenshin77@gmail.com>
2023-01-17 20:54:51 +0800

    Add `Route::inertia()` IDE helper (#413)

--- _ide_helpers.php
+++ _ide_helpers.php
@@ -29,3 +29,25 @@ namespace Illuminate\Foundation\Testing {
         //
     }
 }
+
+namespace Illuminate\Support\Facades {
+
+    /**
+     * @method static self inertia(string $uri, string $component, array $props = [])
+     */
+    class Route
+    {
+        //
+    }
+}
+
+namespace Illuminate\Routing {
+
+    /**
+     * @method self inertia(string $uri, string $component, array $props = [])
+     */
+    class Router
+    {
+        //
+    }
+}
