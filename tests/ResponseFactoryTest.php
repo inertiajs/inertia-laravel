14a85a7db3bb564e3593889eef0572d4c3280786
Vic GUTT <guttvictor@yahoo.fr>
2024-05-29 14:47:49 +0200

    feat: test improvement

--- tests/ResponseFactoryTest.php
+++ tests/ResponseFactoryTest.php
@@ -13,6 +13,7 @@ use Inertia\Tests\Stubs\ExampleMiddleware;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\Request as HttpRequest;
 use Inertia\Response as InertiaResponse;
+use Illuminate\Foundation\Application;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Session\NullSessionHandler;
 use Illuminate\Session\Store;
@@ -204,8 +205,8 @@ class ResponseFactoryTest extends TestCase
 
     public function test_it_allows_the_response_to_be_resolved_from_container(): void
     {
-        app()->bind(InertiaResponse::class, function (): InertiaResponse {
-            return new class ('', []) extends InertiaResponse {
+        app()->bind(InertiaResponse::class, function (Application $app, array $params): InertiaResponse {
+            return new class (...$params) extends InertiaResponse {
                 public function resolveUrl(HttpRequest $request, array $props): string
                 {
                     $requestedFrom = $request->header('x-requested-from');
@@ -226,6 +227,7 @@ class ResponseFactoryTest extends TestCase
         ]);
 
         $response->assertSuccessful();
+        $response->assertJson(['component' => 'User/Edit']);
         $response->assertJson(['url' => '/abc/123#value']);
     }
 }
