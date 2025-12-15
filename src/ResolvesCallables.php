8d52a6753bead9b01a699d40bd142a72668c2a11
Pascal Baljet <pascal@pascalbaljet.nl>
2025-12-15 15:57:37 +0100

    PHP 7.4 fix

--- src/ResolvesCallables.php
+++ src/ResolvesCallables.php
@@ -8,16 +8,21 @@ trait ResolvesCallables
 {
     /**
      * Call the given value if callable and inject its dependencies.
+     *
+     * @param  mixed  $value
+     * @return mixed
      */
-    protected function resolveCallable(mixed $value): mixed
+    protected function resolveCallable($value)
     {
         return $this->useAsCallable($value) ? App::call($value) : $value;
     }
 
     /**
      * Determine if the given value is callable, but not a string.
+     *
+     * @param  mixed  $value
      */
-    protected function useAsCallable(mixed $value): bool
+    protected function useAsCallable($value): bool
     {
         return ! is_string($value) && is_callable($value);
     }
