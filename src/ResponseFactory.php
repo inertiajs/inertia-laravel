d501b1af932cd21115f8f283e63256e21f82f858
Marc Beinder <50760632+onairmarc@users.noreply.github.com>
2026-01-20 05:56:00 -0600

    [3.x] Add Backed Enum Support to Inertia::render() (#770)
    * Add Backed Enum Support to ResponseFactory::render()
    
    * Remove Type Hint in Favor of DocBlock
    
    * Refactor
    
    ---------
    
    Co-authored-by: Pascal Baljet <pascal@pascalbaljet.nl>

--- src/ResponseFactory.php
+++ src/ResponseFactory.php
@@ -265,10 +265,21 @@ class ResponseFactory
     /**
      * Create an Inertia response.
      *
+     * @param  BackedEnum|UnitEnum|string  $component
      * @param  array<array-key, mixed>|\Illuminate\Contracts\Support\Arrayable<array-key, mixed>|ProvidesInertiaProperties  $props
      */
-    public function render(string $component, $props = []): Response
+    public function render($component, $props = []): Response
     {
+        $component = match (true) {
+            $component instanceof BackedEnum => $component->value,
+            $component instanceof UnitEnum => $component->name,
+            default => $component,
+        };
+
+        if (! is_string($component)) {
+            throw new InvalidArgumentException('Component argument must be of type string or a string BackedEnum');
+        }
+
         if (config('inertia.ensure_pages_exist', false)) {
             $this->findComponentOrFail($component);
         }
--- tests/ResponseFactoryTest.php
+++ tests/ResponseFactoryTest.php
@@ -19,8 +19,12 @@ use Inertia\MergeProp;
 use Inertia\OnceProp;
 use Inertia\OptionalProp;
 use Inertia\ResponseFactory;
+use Inertia\Tests\Enums\IntBackedEnum;
+use Inertia\Tests\Enums\StringBackedEnum;
+use Inertia\Tests\Enums\UnitEnum;
 use Inertia\Tests\Stubs\ExampleInertiaPropsProvider;
 use Inertia\Tests\Stubs\ExampleMiddleware;
+use InvalidArgumentException;
 
 class ResponseFactoryTest extends TestCase
 {
@@ -528,6 +532,33 @@ class ResponseFactoryTest extends TestCase
 
         $response = (new ResponseFactory)->render('foo');
         $this->assertInstanceOf(\Inertia\Response::class, $response);
+    }
+
+    public function test_render_accepts_backed_enum(): void
+    {
+        $response = (new ResponseFactory)->render(StringBackedEnum::UsersIndex);
+        $this->assertInstanceOf(\Inertia\Response::class, $response);
+
+        /** @phpstan-ignore-next-line */
+        $getComponent = fn () => $this->component;
+        $this->assertSame('UsersPage/Index', $getComponent->call($response));
+    }
+
+    public function test_render_accepts_unit_enum(): void
+    {
+        $response = (new ResponseFactory)->render(UnitEnum::Index);
+        $this->assertInstanceOf(\Inertia\Response::class, $response);
+
+        /** @phpstan-ignore-next-line */
+        $getComponent = fn () => $this->component;
+        $this->assertSame('Index', $getComponent->call($response));
+    }
+
+    public function test_render_throws_for_non_string_backed_enum(): void
+    {
+        $factory = new ResponseFactory;
+        $this->expectException(InvalidArgumentException::class);
+        $factory->render(IntBackedEnum::Zero);
     }
 
     public function test_share_once_shares_a_once_prop(): void
