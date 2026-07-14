0bf8e1e31a809984e17ba53e6029db2ee410bbe2
goreks-dev <goreks@atomicmail.io>
2026-07-14 05:08:25 -0700

    wip

--- config/inertia.php
+++ config/inertia.php
@@ -29,7 +29,7 @@ return [
 
         'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
 
-        'dev_url' => env('INERTIA_SSR_DEV_URL'),
+        'hot_url' => env('INERTIA_SSR_DEV_URL'),
 
         'ensure_bundle_exists' => (bool) env('INERTIA_SSR_ENSURE_BUNDLE_EXISTS', true),
 
